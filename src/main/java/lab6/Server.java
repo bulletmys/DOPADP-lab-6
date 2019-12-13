bf64ac1c3c9c492b818059848dead5ef32249752
bulletmys <bulletmys@yandex.ru>
2019-12-14 01:38:26 +0300

    Scripted auto-commit on change (2019-12-14 01:38:26) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -1,8 +1,11 @@
 package lab6;
 
+import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.server.AllDirectives;
 
 public class Server extends AllDirectives {
-    public Server(Http http, int port, )
+    public Server(Http http, int port, ActorRef configActor) {
+        
+    }
 }
