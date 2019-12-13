ec24fa56cebdfdec69a01b39bffa458d5444a267
bulletmys <bulletmys@yandex.ru>
2019-12-14 02:27:04 +0300

    Scripted auto-commit on change (2019-12-14 02:27:04) by gitwatch.sh

--- src/main/java/lab6/ZooKeeperService.java
+++ src/main/java/lab6/ZooKeeperService.java
@@ -17,6 +17,11 @@ public class ZooKeeperService {
         zooKeeper = new ZooKeeper("127.0.0.1:2181", 5000, null);
     }
 
+    void makeServer(String url) {
+        zooKeeper.create("/servers/s", url, 
+        )
+    }
+
     void watcher() throws KeeperException, InterruptedException {
         List<String> serverNodes = zooKeeper.getChildren("/servers", watchedEvent -> {
             if (Watcher.Event.EventType.NodeChildrenChanged == watchedEvent.getType()) {
@@ -34,6 +39,6 @@ public class ZooKeeperService {
             server.add(new String(url));
         }
 
-        configActor.tell( ,ActorRef.noSender());
+        configActor.tell(server ,ActorRef.noSender());
     }
 }
