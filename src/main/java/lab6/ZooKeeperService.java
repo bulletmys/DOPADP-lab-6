0e1d2339ea821715d71dd25a4171614143a3c6bb
bulletmys <bulletmys@yandex.ru>
2019-12-14 02:11:49 +0300

    Scripted auto-commit on change (2019-12-14 02:11:49) by gitwatch.sh

--- src/main/java/lab6/ZooKeeperService.java
+++ src/main/java/lab6/ZooKeeperService.java
@@ -10,6 +10,6 @@ public class ZooKeeperService {
 
     ZooKeeperService(ActorRef configActor) {
         this.configActor = configActor;
-        zooKeeper = new ZooKeeper("127.0.0.1:")
+        zooKeeper = new ZooKeeper("127.0.0.1:2050")
     }
 }
