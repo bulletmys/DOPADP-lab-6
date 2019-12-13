98eb08b74e2f7337e832fc73ceece668fbc6702d
bulletmys <bulletmys@yandex.ru>
2019-12-14 02:32:09 +0300

    Scripted auto-commit on change (2019-12-14 02:32:09) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -41,7 +41,7 @@ public class Server extends AllDirectives {
     }
 
     CompletionStage<HttpResponse> getRandReq(String url, int count) {
-        return Patterns.ask(configActor, new RandServer(), Duration.ofMillis(5000)).thenCompose(url -> makeRequest(url))
+        return Patterns.ask(configActor, new RandServer(), Duration.ofMillis(5000)).thenCompose(url -> makeRequest())
     }
 
 
