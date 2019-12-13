fbbe16138491af1fa907eb2925801fea88195de9
bulletmys <bulletmys@yandex.ru>
2019-12-14 02:50:40 +0300

    Scripted auto-commit on change (2019-12-14 02:50:40) by gitwatch.sh

--- src/main/java/lab6/Main.java
+++ src/main/java/lab6/Main.java
@@ -30,7 +30,7 @@ public class Main {
         final ActorMaterializer materializer =
                 ActorMaterializer.create(system);
         Server server = new Server(http, port, configActor);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.createRoute().flow()
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.createRoute().flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost(HOST, PORT),
