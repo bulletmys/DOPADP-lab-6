87bc8b1cedfd23f9127d09b6099a0f75c1a24267
bulletmys <bulletmys@yandex.ru>
2019-12-14 02:33:26 +0300

    Scripted auto-commit on change (2019-12-14 02:33:26) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -34,7 +34,7 @@ public class Server extends AllDirectives {
                             return completeWithFuture(makeRequest(url));
                         }
                         return completeWithFuture()
-                    }))
+                    }));
         })
     }
 
