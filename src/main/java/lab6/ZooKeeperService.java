01e58ceab48d49c7bd41f85ddf8ced10374e2303
bulletmys <bulletmys@yandex.ru>
2019-12-14 02:12:15 +0300

    Scripted auto-commit on change (2019-12-14 02:12:15) by gitwatch.sh

--- src/main/java/lab6/ZooKeeperService.java
+++ src/main/java/lab6/ZooKeeperService.java
@@ -3,13 +3,15 @@ package lab6;
 import akka.actor.ActorRef;
 import org.apache.zookeeper.*;
 
+import java.io.IOException;
+
 public class ZooKeeperService {
 
     ActorRef configActor;
     ZooKeeper zooKeeper;
 
-    ZooKeeperService(ActorRef configActor) {
+    ZooKeeperService(ActorRef configActor) throws IOException {
         this.configActor = configActor;
-        zooKeeper = new ZooKeeper("127.0.0.1:2050", 5000, )
+        zooKeeper = new ZooKeeper("127.0.0.1:2050", 5000, null);
     }
 }
