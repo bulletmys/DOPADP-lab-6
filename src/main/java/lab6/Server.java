63f2735409f79361e077c51da3dcac7566c9ba23
bulletmys <bulletmys@yandex.ru>
2019-12-14 01:49:29 +0300

    Scripted auto-commit on change (2019-12-14 01:49:29) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -19,7 +19,7 @@ public class Server extends AllDirectives {
     Route createRoute() {
         return get(() -> {
             parameter("URL", url ->
-                    parameter())
+                    parameter("count" ))
         })
     }
 
