4e58b38fad836be46eb3b140c6657497a5774162
bulletmys <bulletmys@yandex.ru>
2019-12-14 01:27:01 +0300

    Scripted auto-commit on change (2019-12-14 01:27:01) by gitwatch.sh

--- src/main/java/lab6/ConfigActor.java
+++ src/main/java/lab6/ConfigActor.java
@@ -1,4 +1,10 @@
 package lab6;
 
-public class ConfigActor {
+import akka.actor.AbstractActor;
+
+public class ConfigActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
