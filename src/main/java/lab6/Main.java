bb49304d2afcdbaa5d78ce53f00891ea8dc2da3f
bulletmys <bulletmys@yandex.ru>
2019-12-14 01:22:15 +0300

    Scripted auto-commit on change (2019-12-14 01:22:15) by gitwatch.sh

--- src/main/java/lab6/Main.java
+++ src/main/java/lab6/Main.java
@@ -1,4 +1,41 @@
 package lab6;
 
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.io.IOException;
+
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+
+import java.util.concurrent.CompletionStage;
+
 public class Main {
+    private static final String SERVER_ONLINE_MSG = "Server online at http://localhost:8080/\nPress RETURN to stop...";
+    private static final int PORT = 8080;
+    private static final String HOST = "localhost";
+    private static final String ACTOR_SYSTEM_NAME = "routes";
+
+    public static void main(String[] args) throws IOException {
+        ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = (new HttpClientAsync(system)).httpFlow(materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(HOST, PORT),
+                materializer
+        );
+        System.out.println(SERVER_ONLINE_MSG);
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+    }
 }
